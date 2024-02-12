aa38ea85700a6587ef8944e3d4fd3f2d52cc355c
Leong Weng Peng <110824718+Stereochromyy@users.noreply.github.com>
2024-02-12 13:36:51 +0800

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -1,124 +1,14 @@
-<?php
-    // Bypass the session activation if it has activated
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-
-    include 'dbConn.php';
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paws Print Hub | Malaysia</title>
-    <link rel="stylesheet" href="Main Page.css">
+    <link rel="stylesheet" href="mainPage.css">
 </head>
 
 <body>
-    <header id="header">
-        <a href="Main Page.php">
-            <img src="images/logo.png" alt="Paws Print Hub Logo" id="logo">
-        </a>
-
-        <div id="info"> <!--Name and Motto-->
-            <h2>Paws Print Hub</h2>
-            <p>Connecting Paws, Creating Stories</p>
-        </div>
-
-        <nav id="nav" style="float: left;">
-            <ul>
-                <li><a href="Adopt@Foster.php">Adopt/Foster</a></li>
-                <li><a href="Donation Portal.php">Donation</a></li>
-                <li><a href="volunteer.php">Volunteer</a></li>
-                <li><a href="contactus.php">Contact Us</a></li>
-                <li><img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png" alt="Profile photo">
-                    <ul>
-                        <?php
-                            if (!isset($_SESSION['email']) || $_SESSION['email'] === null || !isset($_SESSION['password']) || $_SESSION['password'] === null){                    
-                        ?>
-                            <li><a href="loginPPH.php" target="_blank">Log in/Sign Up</a></li>
-                        <?php
-                            }
-                            else{
-                        ?>
-                            <li><a href="User Profile Structure.php">View profile</a></li>
-
-                            <li><a href="Log Out.php"> Log Out</a></li>
-                        <?php        
-                            }
-                        ?> 
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <?php
-            if (isset($_SESSION['email']) || isset($_SESSION['password'])) { 
-        ?>
-            <div class = "notification-box" style="margin-left:-25px;">
-                <img src="images/notification.png" alt="Notification">
-
-                <div class = "notification">
-
-                <?php
-                    $query = "SELECT *,
-                        `user`.`userID`,
-                        `adoption`.`approval_status` AS adoption_approval_status,
-                        `foster`.`approval_status` AS foster_approval_status,
-                        `volunteering`.`approval_status` AS volunteering_approval_status
-                        FROM `user`
-                        LEFT JOIN `adoption` ON `user`.`userID` = `adoption`.`userID`
-                        LEFT JOIN `foster` ON `user`.`userID` = `foster`.`userID`
-                        LEFT JOIN `volunteering` ON `user`.`userID` = `volunteering`.`userID` 
-                        WHERE `user`.`email_address` = '$_SESSION[email]' AND `user`.`password` = '$_SESSION[password]'";
-
-                        $result = mysqli_query($connection, $query); 
-                                
-                        if (!$result){
-                            die("Error in SQL query:".mysqli_error($connection));
-                        }
-
-                        // Fetch the results
-                        if (mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                $userID = $row['userID'];
-                                $adoption_status = $row['adoption_approval_status'];
-                                $foster_status = $row['foster_approval_status'];
-                                $volunteering_status = $row['volunteering_approval_status'];
-
-                                // Display approval status for each table if the userID is in the respective table
-
-                                if (!is_null($adoption_status)) {
-                                    echo "Adoption Status: $adoption_status<br>";
-                                }
-
-                                if (!is_null($foster_status)) {
-                                    echo "Foster Status: $foster_status<br>";
-                                }
-
-                                if (!is_null($volunteering_status)) {
-                                    echo "Volunteering Status: $volunteering_status<br>";
-                                }
-
-                                if(is_null($adoption_status) && is_null($foster_status) && is_null($volunteering_status)){
-                                    echo "<p>No Notifications Displayed...</p>";
-                                }
-                                echo "<br>";
-
-                                
-                                        
-                            }
-                        }else {
-                            echo "No results found.";
-                        }
-            }
-                ?>
-                </div>
-            </div>        
-        
-    </header>
+    <?php include 'userNavigationBar.php'; ?>
 
     <main>
         <div id="box">
@@ -231,7 +121,7 @@
         </div>
 
     <footer id="box" style="background-color:snow;">
-        <a href="Main Page.php">
+        <a href="mainPage.php">
             <img src="images/logo.png" alt="Paws Print Hub Logo" id="logo2">
         </a>
         
