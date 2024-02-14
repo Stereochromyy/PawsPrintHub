26612a6e135cae1655327a5ce371b696262d9d6c
LemonadeXD <140675465+LemonadeXD@users.noreply.github.com>
2024-02-14 14:41:12 +0800

    Update volunteer.php

--- volunteer.php
+++ volunteer.php
@@ -1,12 +1,34 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volunteer</title>
 </head>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    body {
+        width: 100%;
+        min-height: 100vh;
+        background: url(images/background.jpg) no-repeat;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        font-size: 17px;
+    }
+
     #box {
         height: auto;
         width: auto;
@@ -15,11 +37,10 @@
         position: relative;
         display: flex;
     }
-
     .volunteer h2 {
-        margin-top: 25px;
-        font-size: 28px;
-        margin-left: 25px;
+    margin-top: 25px;
+    font-size: 28px;
+    margin-left: 25px;
     }
 
     .volunteer p {
@@ -37,6 +58,7 @@
         margin: 25px;
         float: left;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        border-radius: 10px;
     }
 
     #rightimg {
@@ -45,60 +67,69 @@
         margin: 25px;
         float: right;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        border-radius: 10px;
     }
 
     #endsection {
         height: 130px;
         width: auto;
-        border: solid 1px;
-        background-color: snow;
+        background-color: rgb(224, 246, 255);
         position: relative;
         bottom: 0px;
         margin-top: 120px;
+        border-radius: 50% 50% 0 0;
     }
 
     #signupbtnholder {
         height: 175px;
         width: 175px;
         border-radius: 50%;
-        background-color: white;
+        background-color: rgb(224, 246, 255);
         position: absolute;
         bottom: 0px;
         margin-left: 675px;
         text-align: center;
+        margin-top: -20px;
     }
 
     #signupbtn {
         height: 175px;
         width: 175px;
+        background-color: white;
+        border: solid 1px rgb(93, 193, 255);
         border-radius: 50%;
         cursor: pointer;
         text-align: center;
+        margin-top: -20px;
+        transition: 0.5s;
     }
 
     #signupbtn:hover {
-        background-color: lightgrey;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        transform: translateY(-15px);
+        border: solid 1px rgb(34, 102, 249);
     }
-</style>
 
+
+</style>
 <body>
-    <?php include 'userNavigationBar.php'; ?>
+    <?php
+    include("userNavigationBar.php");
+    ?>
 
     <main>
-        <div id="box" class="volunteer" style="display:flow-root;">
+        <div id="box" class= "volunteer" style="display:flow-root;">
             <img src="https://humaneheroes.org/wp-content/uploads/2021/11/Volunteer1.jpg" alt="" id="leftimg">
             <h2><b>Why Volunteer?</b></h2>
             <br>
             <p style="margin-right: 25px;"><b>Helping Animals: </b>
-                You'll directly contribute to the well-being of animals in need. Your assistance can involve feeding, cleaning, walking, and
+                You'll directly contribute to the well-being of animals in need. Your assistance can involve feeding, cleaning, walking, and 
                 socializing with the animals, providing them with care and attention they might not otherwise receive.
             </p>
 
             <p style="margin-right: 25px;"><b>Community Engagement: </b>
-                Volunteering brings you closer to your community. You'll meet like-minded individuals who share a passion for animals and
+                Volunteering brings you closer to your community. You'll meet like-minded individuals who share a passion for animals and 
                 contribute to a cause that's important to you.</p>
-
+            
             <div class="clear"></div><br><br><br><br>
 
             <img src="https://img.freepik.com/free-photo/smiley-woman-playing-shelter-with-dog-waiting-be-adopted_23-2148682992.jpg" alt="" id="rightimg">
@@ -117,34 +148,33 @@
             <img src="https://img.freepik.com/premium-photo/cute-friendly-dog-shelter-is-waiting-friend-home_283617-3933.jpg" alt="" id="leftimg">
             <h2><b>How to Do?</b></h2>
             <br>
-            <p style="margin-right: 25px;">If you are interested, please feel free to contact us through our social media channels or fill out the application form below.
+            <p style="margin-right: 25px;">If you are interested, please feel free to contact us through our social media channels or fill out the application form below. 
                 Our team will get back to you as soon as possible.
             </p>
-
+                
             <div class="clear"></div><br><br><br><br><br><br>
-
+            
             <img src="https://apicms.thestar.com.my/uploads/images/2021/09/04/1281150.jpg" alt="" id="rightimg">
             <h2><b>Where to Do?</b></h2>
             <br>
             <p><b>Paws Print Hub: </b>
-                This is our main location for volunteering. If you are a newbie, don't worry; our team will guide you on where and how you can contribute.
+                This is our main location for volunteering. If you are a newbie, don't worry; our team will guide you on where and how you can contribute. 
                 Remember, volunteering is a two-way street. While you're contributing your time and skills, you're also gaining valuable experiences and making a positive impact. Enjoy your volunteering journey at Paws Print Hub!
             </p>
-
+            
             <div class="clear"></div>
         </div>
-
+        
         <div id="endsection">
             <div id="signupbtnholder">
                 <a href="volunteerForm.php" target="_blank">
-                    <button id="signupbtn">
-                        <h1><i class="fa-solid fa-arrow-right-to-bracket" id="icon"></i></h1>
-                        <h2>Sign Up</h2>
-                    </button>
+                <button id="signupbtn">
+                    <img src="https://icon-library.com/images/signed-icon/signed-icon-14.jpg" alt="sign up icon" height="90px">
+                    <h2>Sign Up</h2>
+                </button>
                 </a>
             </div>
         </div>
     </main>
 </body>
-
 </html>
