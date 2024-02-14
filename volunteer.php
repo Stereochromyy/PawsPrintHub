37dd5eb8da69857d5befa325ec1a36ab1ac0d583
Leong Weng Peng <110824718+Stereochromyy@users.noreply.github.com>
2024-02-14 12:48:27 +0800

    Update volunteer.php

--- volunteer.php
+++ volunteer.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,9 +17,9 @@
     }
 
     .volunteer h2 {
-    margin-top: 25px;
-    font-size: 28px;
-    margin-left: 25px;
+        margin-top: 25px;
+        font-size: 28px;
+        margin-left: 25px;
     }
 
     .volunteer p {
@@ -79,26 +80,25 @@
         background-color: lightgrey;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
     }
-
-
 </style>
+
 <body>
     <?php include 'userNavigationBar.php'; ?>
 
     <main>
-        <div id="box" class= "volunteer" style="display:flow-root;">
+        <div id="box" class="volunteer" style="display:flow-root;">
             <img src="https://humaneheroes.org/wp-content/uploads/2021/11/Volunteer1.jpg" alt="" id="leftimg">
             <h2><b>Why Volunteer?</b></h2>
             <br>
             <p style="margin-right: 25px;"><b>Helping Animals: </b>
-                You'll directly contribute to the well-being of animals in need. Your assistance can involve feeding, cleaning, walking, and 
+                You'll directly contribute to the well-being of animals in need. Your assistance can involve feeding, cleaning, walking, and
                 socializing with the animals, providing them with care and attention they might not otherwise receive.
             </p>
 
             <p style="margin-right: 25px;"><b>Community Engagement: </b>
-                Volunteering brings you closer to your community. You'll meet like-minded individuals who share a passion for animals and 
+                Volunteering brings you closer to your community. You'll meet like-minded individuals who share a passion for animals and
                 contribute to a cause that's important to you.</p>
-            
+
             <div class="clear"></div><br><br><br><br>
 
             <img src="https://img.freepik.com/free-photo/smiley-woman-playing-shelter-with-dog-waiting-be-adopted_23-2148682992.jpg" alt="" id="rightimg">
@@ -117,33 +117,34 @@
             <img src="https://img.freepik.com/premium-photo/cute-friendly-dog-shelter-is-waiting-friend-home_283617-3933.jpg" alt="" id="leftimg">
             <h2><b>How to Do?</b></h2>
             <br>
-            <p style="margin-right: 25px;">If you are interested, please feel free to contact us through our social media channels or fill out the application form below. 
+            <p style="margin-right: 25px;">If you are interested, please feel free to contact us through our social media channels or fill out the application form below.
                 Our team will get back to you as soon as possible.
             </p>
-                
+
             <div class="clear"></div><br><br><br><br><br><br>
-            
+
             <img src="https://apicms.thestar.com.my/uploads/images/2021/09/04/1281150.jpg" alt="" id="rightimg">
             <h2><b>Where to Do?</b></h2>
             <br>
             <p><b>Paws Print Hub: </b>
-                This is our main location for volunteering. If you are a newbie, don't worry; our team will guide you on where and how you can contribute. 
+                This is our main location for volunteering. If you are a newbie, don't worry; our team will guide you on where and how you can contribute.
                 Remember, volunteering is a two-way street. While you're contributing your time and skills, you're also gaining valuable experiences and making a positive impact. Enjoy your volunteering journey at Paws Print Hub!
             </p>
-            
+
             <div class="clear"></div>
         </div>
-        
+
         <div id="endsection">
             <div id="signupbtnholder">
-                <a href="volunteerform.php" target="_blank">
-                <button id="signupbtn">
-                    <h1><i class="fa-solid fa-arrow-right-to-bracket" id="icon"></i></h1>
-                    <h2>Sign Up</h2>
-                </button>
+                <a href="volunteerForm.php" target="_blank">
+                    <button id="signupbtn">
+                        <h1><i class="fa-solid fa-arrow-right-to-bracket" id="icon"></i></h1>
+                        <h2>Sign Up</h2>
+                    </button>
                 </a>
             </div>
         </div>
     </main>
 </body>
+
 </html>
