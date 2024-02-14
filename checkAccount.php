72c972f4bb798c8925a3ad94cd435268e5338faa
Leong Weng Peng <110824718+Stereochromyy@users.noreply.github.com>
2024-02-14 13:15:13 +0800

    Update and rename checkaccount.php to checkAccount.php

---
+++ checkAccount.php
@@ -0,0 +1,7 @@
+<?php
+$account = null;
+if ((isset($_SESSION['email'])) && (isset($_SESSION['password']))) {
+    $account = "user";
+} else {
+    $account = "guest";
+}
--- checkaccount.php
+++
@@ -1,9 +0,0 @@
-<?php
-$account = null;
-if((isset($_SESSION['email'])) && (isset($_SESSION['password']))){
-    $account = "user";
-}
-else{
-    $account = "guest";
-}
-?>
