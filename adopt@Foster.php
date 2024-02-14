d96683c8137dace601f699beeba20861c6f17e5a
LemonadeXD <140675465+LemonadeXD@users.noreply.github.com>
2024-02-14 12:57:06 +0800

    Update adopt@Foster.php

--- adopt@Foster.php
+++ adopt@Foster.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,139 +13,167 @@
 </head>
 
 <body>
-    <?php include 'userNavigationBar.php'; ?>
+
+    <?php
+    include("userNavigationBar.php");
+    ?>
 
     <main>
-        <div id="petbar">
-            <div id="petbuttongroup">
-                <button class="petbutton">All</button>
-                
-                <button class="petbutton">Dog</button>
+        <div id="petbar"></div>
+        <div id="petbuttongroup">
+            <button class="petbutton" id="allfilter">All</button>
+            
+            <button class="petbutton" id="dogfilter">Dog</button>
 
-                <button class="petbutton">Cat</button>
+            <button class="petbutton" id="catfilter">Cat</button>
 
-                <button class="petbutton">Others</button>
-
-            </div>
+            <button class="petbutton" id="otherfilter">Other</button>
         </div>
 
         <div id="main2">
             <?php
                 include 'dbConn.php'; 
 
-                // Step 2: create SQL command - select, insert, update, delete
-                $query = "SELECT * FROM animal_image INNER JOIN animal ON animal.animalID = animal_image.animalID ORDER BY animal.name ASC";
+                    // Step 2: create SQL command - select, insert, update, delete
+                    $query = "SELECT * FROM animal";
 
-                // Step 3: execute query
-                $results = mysqli_query($connection, $query);
+                    // Step 3: execute query
+                    $results = mysqli_query($connection, $query);
 
-                // Step 4: read results
-                while ($row = mysqli_fetch_assoc($results)) {
-                    echo '<div class="petframe">';
-                    echo '<img src="' . $row['animal_image_link'] . '" alt="' . $row['animalimageID'] . '" class="petbox" id="'.$row['animalID'].'">';
-                    echo '<div class="petinfo">';
-                
-                    $birthdate = new DateTime($row['birthday']);
-                    $currentDate = new DateTime();
-                    $age = $currentDate->diff($birthdate);
-                
-                    if ($age->y > 0) {
-                        echo '<p>Name: ' . $row['name'] . '<br> Age: ' . $age->y . ' years</p>';
-                    } else {
-                        echo '<p>Name: ' . $row['name'] . '<br> Age: ' . $age->m . ' months</p>';
-                    }
-                    ?>
+                    // Step 4: read results
+                    while ($row = mysqli_fetch_assoc($results)) {
+                        $animalid = $row["animalID"];
+                        $animalimgquery = "SELECT * FROM animal_image where animalID = '$animalid' ORDER BY animalimageID DESC limit 1";
+                        $imgresult = mysqli_query($connection, $animalimgquery);
+                        while ($imgrow = mysqli_fetch_assoc($imgresult)) {
+                            $animalimg = $imgrow["animal_image_link"];
+                        }
+
+                        $animalspecies = $row["species"];
+
+                        if($animalspecies == "dog"){
+                            $species = "dog";
+                        } else if($animalspecies == "cat"){
+                            $species = "cat";
+                        } else{
+                            $species = "other";
+                        }
+                        
+                        echo '<div class="petframe '.$species.'">';
+                        echo '<img src="'. $animalimg .'" class="petbox" id="'.$animalid.'">';
+                        echo '<div class="petinfo">';
+                    
+                        $birthdate = new DateTime($row['birthday']);
+                        $currentDate = new DateTime();
+                        $age = $currentDate->diff($birthdate);
+                    
+                        if ($age->y > 0) {
+                            echo '<p>Name: ' . $row['name'] . '<br> Age: ' . $age->y . ' years </p>';
+                        } else {
+                            echo '<p>Name: ' . $row['name'] . '<br> Age: ' . $age->m . ' months </p>';
+                        }
+                        ?>
                         </br>
-                        <p class="view" data-animalid="<?php echo $row['animalID']; ?>"> View More ￫ </p>
-                        <!-- The Modal -->
-                        <div id="myModal_<?php echo $row['animalID']; ?>" class="modal">
-
-                            <!-- Modal content -->
-                            <div class="modal-content">
-                                <div class="close">&times;</div>
-                                <div class="petprofile">
-                                    <div class="petprofile1">
-                                        <img src="<?php echo $row['animal_image_link'] ?>" alt="Animal Image">
-                                    </div>
-                                </div>
-                                <div class="petdetails">
-                                    <?php
-                                        echo '<b><u>Pet Basic Info</u></b>';
-                                        echo '<p>'.'Name: '. $row['name'].'</p>';
-                                        if ($age->y > 0) {
-                                            echo 'Age: ' . $age->y . ' years</p>';
-                                        } else {
-                                            echo 'Age: ' . $age->m . ' months</p>';
-                                        }
-                                        echo '<p>'.'Species: '. $row['species'].'</p>';
-                                        echo '<p>'.'Breed: '. $row['breed'].'</p>';
-                                        echo '<p>'.'Weight: '. $row['weight'].' kg </p><br>';
-                                        echo '<b><u>Health Status</u></b>';
-                                        echo '<p>'.'Vaccination status: '. $row['vaccination_status'].'</p>';
-                                        echo '<p>'.'Nurturing status: '. $row['nurturing_status'].'</p><br>';
-                                        echo '<b><u>Additional Info</u></b>';
-                                        echo '<p> -'.$row['additional_info'].'</p>';
-                                    ?>
-                                </div>
-                                <div class="position">
-                                    <div class="button" style="background-color: #98E4FF;">
-                                        <img src="https://cdn-icons-png.flaticon.com/512/1883/1883829.png" alt="Adopt"><h2>ADOPT</h2>
-                                    </div>
-                                    <div class="button" style="margin-left:-80px; background-color: #A1EEBD">
-                                        <img src="https://cdn-icons-png.flaticon.com/512/3769/3769065.png" alt="Foster"><h2>FOSTER</h2>
-                                    </div>
-                                </div>
-                                
-                            </div>
-
-                        </div>
-                    <?php
-                    echo '</div>';
-                    echo '</div>'; 
-                }
+                        <a href = "petdetailpage.php?adopt_animalID=<?php echo $animalid; ?>">
+                            <button class="view" id="viewpet"> View More ￫ </button>
+                        </a>
+                        <?php
+                        echo '</div>';
+                        echo '</div>'; 
+                    }
+                    
 
                 // Step 5: close connection
                 mysqli_close($connection);
-            ?>
+                        ?>
         </div>
-        <script>
-            // Get all the buttons with class "view"
-            var buttons = document.querySelectorAll('.view');
-
-            // Function to open the modal
-            function openModal(animalID) {
-                var modal = document.getElementById("myModal_" + animalID);
-                modal.style.display = "block";
-            }
-
-            // Function to close the modal
-            function closeModal(animalID) {
-                var modal = document.getElementById("myModal_" + animalID);
-                modal.style.display = "none";
-            }
-
-            buttons.forEach(function (button) {
-                var animalID = button.getAttribute('data-animalid');
-                var modal = document.querySelector("#myModal_" + animalID);
-
-                button.addEventListener('click', function () {
-                    openModal(animalID);
-                });
-
-                var close = modal.querySelector(".close");
-
-                close.addEventListener('click', function () {
-                    closeModal(animalID);
-                });
-
-                window.addEventListener('click', function (event) {
-                    if (event.target == modal) {
-                        closeModal(animalID);
-                    }
-                });
-            });
-
-        </script>
     </main>
+
+    <script>
+    var viewbtn = document.getElementById("viewpet");
+    viewbtn.onclick = function(){
+        window.location.href = "petdetailpage.php?";
+    }
+
+    // Setting up variable for buttons
+    var allbtn = document.getElementById("allfilter");
+    var dogbtn = document.getElementById("dogfilter");
+    var catbtn = document.getElementById("catfilter");
+    var otherbtn = document.getElementById("otherfilter");
+
+    // Setting up variable for pet frame with species as their id
+    var dog = document.getElementsByClassName("dog");
+    var cat = document.getElementsByClassName("cat");
+    var other = document.getElementsByClassName("other");
+
+    allbtn.onclick = function(){
+        allbtn.style.backgroundColor = "rgb(214, 237, 255)";
+        dogbtn.style.backgroundColor = "white";
+        catbtn.style.backgroundColor = "white";
+        otherbtn.style.backgroundColor = "white";
+
+        for(var i=0; i<dog.length; i++){
+            dog[i].style.display = "block";
+        }
+        for(var i=0; i<cat.length; i++){
+            cat[i].style.display = "block";
+        }
+        for(var i=0; i<other.length; i++){
+            other[i].style.display = "block";
+        }
+    }
+    
+    dogbtn.onclick = function(){
+        allbtn.style.backgroundColor = "white";
+        dogbtn.style.backgroundColor = "rgb(214, 237, 255)";
+        catbtn.style.backgroundColor = "white";
+        otherbtn.style.backgroundColor = "white";
+
+        for(var i=0; i<dog.length; i++){
+            dog[i].style.display = "block";
+        }
+        for(var i=0; i<cat.length; i++){
+            cat[i].style.display = "none";
+        }
+        for(var i=0; i<other.length; i++){
+            other[i].style.display = "none";
+        }
+    }
+
+    catbtn.onclick = function(){
+        allbtn.style.backgroundColor = "white";
+        dogbtn.style.backgroundColor = "white";
+        catbtn.style.backgroundColor = "rgb(214, 237, 255)";
+        otherbtn.style.backgroundColor = "white";
+
+        for(var i=0; i<dog.length; i++){
+            dog[i].style.display = "none";
+        }
+        for(var i=0; i<cat.length; i++){
+            cat[i].style.display = "block";
+        }
+        for(var i=0; i<other.length; i++){
+            other[i].style.display = "none";
+        }
+    }
+
+    otherbtn.onclick = function(){
+        allbtn.style.backgroundColor = "white";
+        dogbtn.style.backgroundColor = "white";
+        catbtn.style.backgroundColor = "white";
+        otherbtn.style.backgroundColor = "rgb(214, 237, 255)";
+
+        for(var i=0; i<dog.length; i++){
+            dog[i].style.display = "none";
+        }
+        for(var i=0; i<cat.length; i++){
+            cat[i].style.display = "none";
+        }
+        for(var i=0; i<other.length; i++){
+            other[i].style.display = "block";
+        }
+    }
+</script>
+
 </body>
 </html>
