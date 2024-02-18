d35ea100f0570c639d96f3708834afd12db3bf3b
LemonadeXD <140675465+LemonadeXD@users.noreply.github.com>
2024-02-18 12:42:46 +0800

    Update formManagement.php

--- formManagement.php
+++ formManagement.php
@@ -5,55 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Management</title>
+    <link rel="stylesheet" href="formManagement.css">
 </head>
-<style>
-    .fmbox {
-        margin: auto;
-        height: auto;
-        width: 70%;
-        margin-top: 125px;
-        margin-bottom: -90px;
-        background-color: #f9f9f9;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        border-radius: 8px;
-        position: relative;
-        padding: 50px;
-        border-radius: 25px;
-        border: 2px solid;
-        transition: 0.5s;
-    }
-
-    .fmbox:hover{
-        box-shadow: lightslategray 4px 4px 2px;
-        transform: translateY(8px);
-        transition: ease-in 0.3s;
-    }
-
-    .fmbox a {
-        text-decoration: none;
-        text-align: center;
-        font-size: large;
-        cursor: pointer;
-    }
-</style>
-
 <body>
     <?php include 'adminNavBar.php'; ?>
-
     <main>
-        <div class="fmbox">
+        <div id="filler"></div> 
+        <div id="formbtngroup">
             <a href="adminAdoptionApplication.php">
-                <h2>View Adoption Application</h2>
+                <div class="fmbox" id="adoptformbtn">
+                    <div class="decoration1"></div>
+                    <div class="decoration2"></div>
+                    <div class="decoration3"></div>
+                    <h2>View Adoption Application</h2>  
+                </div>
             </a>
-        </div>
-        <div class="fmbox">
+
             <a href="adminFosterApplication.php">
-                <h2>View Fostering Application</h2>
+                <div class="fmbox" id="fosterformbtn">
+                    <div class="decoration1"></div>
+                    <div class="decoration2"></div>
+                    <div class="decoration3"></div>
+                    <h2>View Fostering Application</h2>
+                </div>
             </a>
-        </div>
-        <div class="fmbox">
+
             <a href="adminVolunteeringApplication.php">
-                <h2>View Volunteer Application</h2>
+                <div class="fmbox" id="volunteerformbtn">
+                    <div class="decoration1"></div>
+                    <div class="decoration2"></div>
+                    <div class="decoration3"></div>
+                    <h2>View Volunteer Application</h2>    
+                </div>
             </a>
         </div>
     </main>
